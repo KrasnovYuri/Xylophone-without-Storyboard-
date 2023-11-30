53ade1d00409e723138e729162c835ec9bd77410
KrasnovYuri <krasnov1yuri@gmail.com>
2023-11-30 19:29:42 +0300

    add stackView

--- Xylophone (without Storyboard)/ViewController.swift
+++ Xylophone (without Storyboard)/ViewController.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let nameButtons = ["C", "D", "E", "F", "G", "A", "B"]
+    private let buttonStackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        setViews()
+        setConstraints()
+        buttonStackView.backgroundColor = .cyan
     }
 
 
 }
 
+extension ViewController {
+    private func setViews() {
+        view.addSubview(buttonStackView)
+    }
+    
+    private func setConstraints() {
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttonStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            buttonStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
