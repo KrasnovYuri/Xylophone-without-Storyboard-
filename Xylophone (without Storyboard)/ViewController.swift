b15ddb40d85ccc8ed924f63e24dc2136fd1523ef
KrasnovYuri <krasnov1yuri@gmail.com>
2023-11-30 21:21:41 +0300

    add buttons

--- Xylophone (without Storyboard)/ViewController.swift
+++ Xylophone (without Storyboard)/ViewController.swift
@@ -17,7 +17,31 @@ class ViewController: UIViewController {
         
         setViews()
         setConstraints()
-        buttonStackView.backgroundColor = .cyan
+        createButtons()
+    }
+    
+    private func createButtons() {
+        for (index, nameButton) in nameButtons.enumerated() {
+            let multiplierWidth = 0.97 - (0.03 * Double(index))
+            createButton(name: nameButton, width: multiplierWidth)
+        }
+    }
+    
+    private func createButton(name: String, width: Double) {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(name, for: .normal)
+        button.addTarget(self, action: #selector(buttonsTapped), for: .touchUpInside)
+        
+        buttonStackView.addArrangedSubview(button)
+        
+        button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: width).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        button.backgroundColor = .red
+    }
+    
+    @objc private func buttonsTapped(_ sender: UIButton) {
+        print(sender.currentTitle)
     }
 
 
@@ -25,7 +49,12 @@ class ViewController: UIViewController {
 
 extension ViewController {
     private func setViews() {
+        view.backgroundColor = .white
         view.addSubview(buttonStackView)
+        buttonStackView.alignment = .center//Делаем по центру
+        buttonStackView.axis = .vertical //Делаем стек вертикальным
+        buttonStackView.spacing = 10
+        buttonStackView.distribution = .fillEqually //Заполнение
     }
     
     private func setConstraints() {
